bd72d8f09eeaa7508bf1465981be2f1f74a29dcf
Vivek Upadhyay <145438627+Its-vivek1@users.noreply.github.com>
2024-10-03 18:24:09 +0530

    Add files via upload

---
+++ day6mediumques.HOSTELROOM.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T; // Read the number of test cases
+
+    while (T--) {
+        int N, X;
+        cin >> N >> X; 
+
+        vector<int> A(N);
+        for (int i = 0; i < N; ++i) {
+            cin >> A[i]; 
+        }
+
+        int current_people = X;
+        int max_people = X; 
+
+        for (int i = 0; i < N; ++i) {
+            current_people += A[i]; 
+            max_people = max(max_people, current_people);  
+        }
+
+        cout << max_people << endl; 
+    }
+
+    return 0;
+}
