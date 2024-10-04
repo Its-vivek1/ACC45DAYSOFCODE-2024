36e50aac2f5c0c24c4e58b86212d460338fc82dd
Vivek Upadhyay <145438627+Its-vivek1@users.noreply.github.com>
2024-10-04 17:56:03 +0530

    Add files via upload

---
+++ day7easyques.JASSIGNMENTS.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T; 
+    cin >> T;
+
+    while (T--) {
+        int X; // Start time
+        cin >> X;
+
+        if (X + 3 <= 10) {
+            cout << "Yes" << endl;
+        } else {
+            cout << "No" << endl;
+        }
+    }
+
+    return 0;
+}
