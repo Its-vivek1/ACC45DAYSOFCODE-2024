69678241fc0ecf8768e2af61755b0a17a7bb2fde
Vivek Upadhyay <145438627+Its-vivek1@users.noreply.github.com>
2024-10-13 12:10:54 +0530

    Add files via upload

---
+++ day16easyques.NOTEBOOK.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T; 
+    cin >> T; 
+
+    while (T--) {
+        int N; 
+        cin >> N; 
+
+        int notebooks = N * 10;
+
+        cout << notebooks << endl;
+    }
+
+    return 0;
+}
