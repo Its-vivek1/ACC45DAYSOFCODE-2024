95249c29455edc2be3cac3a8fd80519b24bdebdd
Vivek Upadhyay <145438627+Its-vivek1@users.noreply.github.com>
2024-11-05 19:05:25 +0530

    Add files via upload

---
+++ day34easyques.BLACKJACK.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;  
+    cin >> T;
+
+    while (T--) {
+        int A, B;
+        cin >> A >> B;
+
+        int C = 21 - (A + B);  
+        
+        if (C >= 1 && C <= 10) {
+            cout << C << endl; 
+        } else {
+            cout << -1 << endl; 
+        }
+    }
+
+    return 0;
+}
